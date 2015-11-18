58ca49b3da75513c83d4842173be012bcf2656c2
Kirill Novik <jimmynovik@gmail.com>
2015-11-18 19:27:54 +0000

    Added the Login Functionality with Facebook and Google

--- temp/app.js
+++ temp/app.js
@@ -15,6 +15,7 @@ var express = require('express')
   , passport = require('passport')
   , User = require('./models/user').User
   , GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+  , FacebookStrategy = require('passport-facebook').Strategy
   ;/*
   , passport = require('passport')
   , passportLocal = require('passport-local')
@@ -89,6 +90,46 @@ passport.use(new GoogleStrategy({
         });
     }));
 
+passport.use(new FacebookStrategy({
+    clientID: "207981646200067",
+    clientSecret: "db1f9259916e16e94be9e2c6a02fa6de",
+    callbackURL: "https://nodeauthentication-kino6052.c9.io/auth/facebook/callback"
+  },
+  function(token, refreshToken, profile, done) {
+
+        // make the code asynchronous
+        // User.findOne won't fire until we have all our data back from Google
+        process.nextTick(function() {
+
+            // try to find the user based on their google id
+            User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+                if (err)
+                    return done(err);
+
+                if (user) {
+                    console.log("[STATUS] User is Found (Line 63)");
+                    // if a user is found, log them in
+                    return done(null, user);
+                } else {
+                    // if the user isnt in our database, create a new user
+                    var newUser          = new User();
+                    console.log(profile);
+                    // set all of the relevant information
+                    newUser.facebook.id    = profile.id;
+                    newUser.facebook.token = token;
+                    newUser.facebook.name  = profile.displayName;
+                    //newUser.google.email = profile.emails[0].value; // pull the first email
+
+                    // save the user
+                    newUser.save(function(err) {
+                        if (err)
+                            throw err;
+                        return done(null, newUser);
+                    });
+                }
+            });
+        });
+    }));
 
 
 // all environments
@@ -137,6 +178,24 @@ app.get('/auth/google/callback',
       successRedirect: '/'
   }));
   
+// Redirect the user to Facebook for authentication.  When complete,
+// Facebook will redirect the user back to the application at
+//     /auth/facebook/callback
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+app.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { successRedirect: '/',
+                                      failureRedirect: '/login' }));
+  
+app.get('/logout', function(req, res){
+  req.logout();
+  res.redirect('/login');
+});
+
 
 function isLoggedIn(req, res, next) {
   console.log("isAuthenticated? " + req.isAuthenticated());
